ea5997726abe98ca934cbedc917ceae88cbd3ea8
Roberto de Carvalho <rcesar221@hotmail.com>
2026-08-18 11:02:35 +0100

    Added colors to tailwind

--- packages/web/tailwind.config.cjs
+++ packages/web/tailwind.config.cjs
@@ -7,12 +7,24 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                "danger": "#ff0000",
-                "neutral": "#b9804a",
-                "primary": "#ffd700",
-                "secondary": "#002f6c",
-                "success": "#389b50",
-                "warning": "#f0b639"
+                //#region Colors
+                "accent": "#9b0808",
+
+                "primary": "#0b5196",
+                "primary-light": "#61a7ec",
+                "primary-dark": "#05223d",
+
+                "secondary": "#fbe200",
+                "secondary-light": "#fff388",
+                "secondary-dark": "#3f3900",
+
+                "neutral": "#a2a2a2",
+                "neutral-dark": "#1a1a1a",
+                "neutral-light": "#fff",
+
+                "success": "#8dd751",
+                "success-dark": "#1a3404"
+                //#endregion
             }
         }
     },
